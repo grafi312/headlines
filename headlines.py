90c4c2c2176b6c414806b091156f90dd2e48c7d3
AlexV <grafi312@gmail.com>
2018-02-08 09:34:09 +0600

    added template for home page and display list of articles of the publication

--- headlines.py
+++ headlines.py
@@ -1,6 +1,6 @@
 import feedparser
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -15,25 +15,8 @@ RSS_FEEDS = {
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed["entries"][0]
 
-    return """
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Headlines</title>
-</head>
-<body>
-    <h1>Headlines</h1>
-    <h4>{0}</h4>
-    <i>{1}</i>
-    <p>{2}</p>
-</body>
-</html>
-    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template("home.html", articles=feed["entries"])
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
